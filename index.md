58ea1549d0342ea52e2934dda4e7ab53e1a92846
Kimon Kar <kimon.kar@gmail.com>
2020-09-21 10:40:30 +0300

    Add ITC3121 and PH3005

--- index.md
+++ index.md
@@ -4,3 +4,9 @@
 - [ITC 2101: Principles of Wireless, IoT, and Mobile Networks](ITC2101/ITC2101.html)
 
 - [ITC 3106: Mathematics for Computing](ITC3106/ITC3106.html)
+
+- [ITC 3121: Computer Networks Modelling and Analysis](ITC3121/ITC3121.html)
+
+---
+
+- [PH 3005: Business Ethics](PH3005/PH3005.html)
--- strip.py
+++ strip.py
@@ -1,11 +1,15 @@
+#!/usr/bin/env python3
 import sys
 if len(sys.argv) != 2:
-    print('Expected format: python strip.py [file]')
-    sys.exit(1)
+	print('Expected format: strip.py [file]')
+	sys.exit(1)
 
 lines = []
 with open(sys.argv[1]) as old:
-    lines = old.readlines()
+	lines = old.readlines()
+	if lines[6].find('style.css') != -1:
+		print('File {} already stripped'.format(sys.argv[1]))
+		sys.exit(2)
 
 with open(sys.argv[1], 'w') as new:
-    new.writelines(lines[0:6]+['\t\t\t<link rel="stylesheet" href="../style.css">\n']+lines[260:-1])
+	new.writelines(lines[0:6]+['\t\t\t<link rel="stylesheet" href="../style.css">\n']+lines[260:-1])
